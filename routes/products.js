84d29b3f7a06eadc6ef7ef7794c18b1e6806dcad
Curiosity10 <alexds3462@gmail.com>
2018-05-25 00:48:09 +0300

    Change url

--- routes/products.js
+++ routes/products.js
@@ -15,8 +15,8 @@ router.get('/', function (req, res, next) {
       .skip(perPage * page)
       .exec(function (err, products) {
         pages = Math.floor(count / perPage);
-        let prevUrl = `http://localhost:3000/products?page=${page - 1}`;
-        let nextUrl = `http://localhost:3000/products?page=${page + 1}`;
+        let prevUrl = `https://vue-course-project.herokuapp.com/products?page=${page - 1}`;
+        let nextUrl = `https://vue-course-project.herokuapp.com/products?page=${page + 1}`;
         let response = {
           products,
           currentPage: page,

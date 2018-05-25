bb261924f6d9c77ffce8c910ba030aac27f3f984
Curiosity10 <alexds3462@gmail.com>
2018-05-25 09:05:33 +0300

    delete 500 error

--- app.js
+++ app.js
@@ -26,16 +26,5 @@ app.use(morgan('dev'));
 
 app.use('/products', products);
 
-// error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
 
